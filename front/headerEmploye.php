7c909aa11b6925196106cdab4d162a2c93dc69ae
RaSta89 <raphaelroumieux@orange.fr>
2025-01-21 16:09:01 +0100

    Création : header selon le rôle, scriptHeader, styleHeader

---
+++ front/headerEmploye.php
@@ -0,0 +1,37 @@
+<head>
+    <link href="/style/styleHeader.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</head>
+
+<header>
+    <nav class="navbar navbar-expand-lg" style="background-color: #427AA1;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php">EcoRide</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Covoiturages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Employé(e)</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Deconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ script/scriptHeader.php
@@ -0,0 +1,26 @@
+<?php 
+
+if (!isset($_SESSION["user"]['role'])) {
+    $_SESSION['user']['role'] = '0';
+}
+
+ $utilisateur = $_SESSION ["user"]["role"];
+
+
+if ($utilisateur == 1){
+    require_once "front/headerAdmin.php";
+} 
+
+if ($utilisateur == 2){
+    require_once "front/headerEmploye.php";
+}
+
+if ($utilisateur == 3){
+    require_once "front/headerUtilisateur.php";
+}
+
+if ($utilisateur == 0){
+    require_once "front/header.php";
+}
+
+?>
