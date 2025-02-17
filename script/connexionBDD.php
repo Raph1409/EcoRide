43ca30fb3521506b46e3b6fd0fdda8cf90d5c6dc
RaSta89 <raphaelroumieux@orange.fr>
2025-02-17 15:52:20 +0100

    ajout ecoride.sql

--- script/connexionBDD.php
+++ script/connexionBDD.php
@@ -1,15 +1,29 @@
 <?php
-    $dsn = "mysql:host=127.0.0.1;port=3307;dbname=ecoride;";
-    $username = "user_php";
-    $password = "4g8rkkEmn4JH89P"; 
+$url = getenv('JAWSDB_URL');
 
-    try{
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if ($url) {
+    // Environnement Heroku (JawsDB)
+    $dbparts = parse_url($url);
     
-    
-    }catch(PDOException $e) {
-        echo "Erreur De connexion à la base de données : ". $e->getMessage();
-    }   
-    
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'], '/');
+    $port = 3306;
+} else {
+    // Environnement local
+    $hostname = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $database = "ecoride";
+    $port = 3307;
+}
+
+// Connexion à la base de données avec PDO
+try {
+    $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
 ?>
